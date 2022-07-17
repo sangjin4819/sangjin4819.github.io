d36ab5d7cf100b1ecae888be765666471d81cb3f
sangjin4819 <79075426+sangjin4819@users.noreply.github.com>
2022-07-18 05:16:35 +0900

    Create clock.js

---
+++ clock.js
@@ -0,0 +1,17 @@
+const clock = document.getElementById("clock")
+
+
+function time(){
+  const date = new Date() 
+
+  const hours =String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, 0);
+
+  clock.innerText = `${hours} : ${minutes} : ${seconds}`;
+
+}
+
+
+time()
+setInterval(time, 1000)
